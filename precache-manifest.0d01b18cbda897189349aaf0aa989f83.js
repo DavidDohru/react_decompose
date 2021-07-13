07a7b0633057a42806256d55bc31f78a43dc0c1b
David Dohru <tronsmartmy@gmail.com>
2021-07-13 19:35:55 +0300

    Updates

---
+++ precache-manifest.0d01b18cbda897189349aaf0aa989f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555aa462720bc5dec905edc44c6245aa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "38065067f0cf86095dfb",
+    "url": "./static/css/main.9ac1a6ac.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38065067f0cf86095dfb",
+    "url": "./static/js/main.3af5076c.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
